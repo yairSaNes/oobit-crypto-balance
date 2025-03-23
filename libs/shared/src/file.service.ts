d350813664f84e9338d0f518e9a3dbc1e5fc49bc
Yair Sa-nes <Yair.sa-nes@harmonicinc.com>
2025-03-23 16:48:13 +0200

    fix fileservice

--- libs/shared/src/file.service.ts
+++ libs/shared/src/file.service.ts
@@ -5,35 +5,47 @@ import * as path from 'path';
 
 @Injectable()
 export class FileService{
+  // public async readJsonFile2<T>(filePath: string): Promise<T> {
+  //   try{
+  //     if(!fs.existsSync(filePath)){
+  //       console.warn(`File ${filePath} not found. Creating new file...`);
+  //       await this.writeJsonFile(filePath, []);
+  //       return [] as T;
+  //     }
+  //     const content = await fs.promises.readFile(filePath, 'utf8');
+  //     const data = JSON.parse(content);
+      
+  //     return (Array.isArray(data) ? data : []) as T;
+  //   }
+  //   catch (err){
+  //     throw err;
+  //   }
+  // }
+
   public async readJsonFile<T>(filePath: string): Promise<T> {
     try{
-      if(!fs.existsSync(filePath)){
-        console.warn(`File ${filePath} not found. Creating new file...`);
+      const data = await fs.promises.readFile(filePath, 'utf-8');
+      return JSON.parse(data) as T;
+    }
+    catch (err){
+      if(err.code === 'ENOENT'){
         await this.writeJsonFile(filePath, []);
         return [] as T;
       }
-      const content = await fs.promises.readFile(filePath, 'utf8');
-      const data = JSON.parse(content);
-      
-      return (Array.isArray(data) ? data : []) as T;
-    }
-    catch (err){
       throw err;
     }
   }
 
   public async writeJsonFile<T>(filePath: string, data: T): Promise<void> {
-
-  try {
-    // Ensure directory exists before writing
-    const dir = path.dirname(filePath);
-    if (!fs.existsSync(dir)) {
-      fs.mkdirSync(dir, { recursive: true }); // Create directory if needed
-    }
-
-    await fs.promises.writeFile(filePath, JSON.stringify(data, null, 2), 'utf8');
-  } catch (error) {
-    console.error(`Error writing to ${filePath}:`, error);
-  }  }
-
+    try {
+      const dir = path.dirname(filePath);
+      if (!fs.existsSync(dir)) {
+        fs.mkdirSync(dir, { recursive: true });
+      }
+      await fs.promises.writeFile(filePath, JSON.stringify(data, null, 2), 'utf8');
+    } 
+    catch (error) {
+      console.error(`Error writing to ${filePath}:`, error);
+    }  
+  }
 }
