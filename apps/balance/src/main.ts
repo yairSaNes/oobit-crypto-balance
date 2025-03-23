578d6a64c183604e0581dc141f5fd8cc550ade55
Yair Sa-nes <Yair.sa-nes@harmonicinc.com>
2025-03-23 12:12:07 +0200

    added main

--- apps/balance/src/main.ts
+++ apps/balance/src/main.ts
@@ -3,6 +3,7 @@ import { BalanceServiceModule } from './balance.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(BalanceServiceModule);
-  await app.listen(process.env.port ?? 3000);
+  await app.listen(3001);
+  console.log('Balance Service running on http://localhost:3001');
 }
 bootstrap();
