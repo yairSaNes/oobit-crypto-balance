24eefdf069b04e587cc6507049d42b4adef304f8
Yair Sa-nes <Yair.sa-nes@harmonicinc.com>
2025-03-24 21:41:36 +0200

    added cache expiration logic

--- apps/rate/src/rate.service.ts
+++ apps/rate/src/rate.service.ts
@@ -22,14 +22,16 @@ export class RateService {
 
   private supportedCoins: string[] = [];
   private supportedCurrencies: string[] = [];
-  private coinRates: Record<string, CoinRate> = {};  
+  private cachedCoinRates: Record<string, CoinRate> = {}; 
+  private cacheExpiry: number = 0;
+  private readonly CACHE_TTL_MS = 60*1000; 
 
   constructor(private readonly fileService: FileService){}
 
   async onModuleInit(): Promise<void> {
-    this.logger.log('Restoring supported coins and currencies from disk...');
-    await this.restoreSupportedData();
-    await this.fetchRates();
+    this.logger.log('Restoring supported coins and currencies from file...');
+    await this.restoreSupportedData(); //restore data from file after crash
+    await this.fetchRates();  //get rates from coin gecho api
   }
 
   getSupportedCoins(): string[] {
@@ -39,48 +41,49 @@ export class RateService {
   getSupportedCurrencies(): string[] {
     return this.supportedCurrencies;
   }
-
+  
+  //changes supportedCoins array
   setSupportedCoins(supportedCoins: string[]): void {
     this.supportedCoins = [...new Set(supportedCoins)];
     this.logger.log(`updated supported coins with ${supportedCoins.length} coins`);
   }
 
+  //changes supportedCurrencies array
   setSupportedCurrencies(supportedCurrencies: string[]): void {
     this.supportedCurrencies = [...new Set(supportedCurrencies)];
     this.logger.log(`updated supported currencies with ${supportedCurrencies.length} currencies`);
   }
 
+  //adds a coin to supportedCoins
   addCoin(coin: string): void {
     if (!this.supportedCoins.includes(coin)) {
       this.supportedCoins.push(coin);
     }
   }
 
+  //adds a currency to supportedCurrencies
   addCurrency(currency: string): void {
     if (!this.supportedCurrencies.includes(currency)) {
       this.supportedCurrencies.push(currency);
     }
   }
 
-  // getCoinRates(coin: string): CoinRate | undefined {
-  //   return this.coinRates[coin];
-  // }
-
-  async fetchRates(): Promise<void>{
+  //gets rates for all supported coins and currencies and updates the cache 
+  async fetchRates(): Promise<void>{ 
     if (this.supportedCoins.length === 0 || this.supportedCurrencies.length === 0) {
       this.logger.warn('No supported coins or currencies to fetch rates for.');
       return;
     }
-    const coins = this.supportedCoins.join(',');
-    const currencies = this.supportedCurrencies.join(',');
     try{
+      const coins = this.supportedCoins.join(',');
+      const currencies = this.supportedCurrencies.join(',');
       const response = await axios.get(this.COINGECKO_API_URL, {
         params: {
           ids: coins,
           vs_currencies: currencies,
         },
       });
-      this.cacheRates(response.data);
+      this.cacheRates(response.data); //updates cache
       this.logger.log(`Fetched rates for ${Object.keys(response.data).length} coins.`);
     }
     catch (error) {
@@ -95,17 +98,22 @@ export class RateService {
   }
 
   private cacheRates(rates: any): void {
+    //update cache
     Object.keys(rates).forEach(coin => {
-      this.coinRates[coin] = { ...this.coinRates[coin], ...rates[coin] };
+      this.cachedCoinRates[coin] = { ...this.cachedCoinRates[coin], ...rates[coin] };
     });
+    //update cache expiration
+    this.cacheExpiry = Date.now() + this.CACHE_TTL_MS;
   }
   
+  //cron job to fetch rates
   @Cron(CronExpression.EVERY_30_SECONDS)
-  async handleCron() {
+  async cronFetch() {
     this.logger.log('Fetching latest crypto rates...');
     await this.fetchRates();
   }
 
+  //stores supportedCoins & supportedcurrencies to file for crash failsafe
   @Cron(CronExpression.EVERY_5_MINUTES)
   async saveSuportedDataToDisk(): Promise<void>{
     const data = {
@@ -117,6 +125,7 @@ export class RateService {
     this.logger.log(`Saved ${this.supportedCoins.length} supported coins and ${this.supportedCurrencies.length} supported currencies to disk`);
   }
 
+  // restore supported data from file
   async restoreSupportedData(): Promise<void> {
     try{
       const data = await this.fileService.readJsonFile<SupportedData>(this.filePath);
@@ -127,22 +136,25 @@ export class RateService {
       this.supportedCoins = data.supportedCoins;
       this.supportedCurrencies = data.supportedCurrencies;
       const restoredAt = new Date(data.timeStamp).toISOString();
-      this.logger.log(`Supported coins and currencies restored from disk (Last saved: ${restoredAt})`);
+      this.logger.log(`Supported coins and currencies restored from file (Last saved: ${restoredAt})`);
     }
     catch(error){
-      this.logger.error('Failed to restore supported coins and currencies from disk', error);
+      this.logger.error('Failed to restore supported coins and currencies from file', error);
     }
   }
 
+  //get price for 1 crypto coin with 1 currency
   async getCryptoPrice(coin: string, currency: string = 'usd', skipCache: boolean = false): Promise<number>{
-    if(!skipCache){
+    //if skipCache wasn't selected or cache isn't expired
+    if(!skipCache && Date.now()<this.cacheExpiry){
       //search cache for rate: 
-      const cachedRate = this.coinRates[coin]?.[currency];
+      const cachedRate = this.cachedCoinRates[coin]?.[currency];
       if (cachedRate){
         this.logger.log(`Rate for ${coin} in ${currency} found in cache: ${cachedRate}`);
         return cachedRate;
       } 
     }
+    //adds requested coin and currency to supportedData
     this.addCoin(coin);
     this.addCurrency(currency);
     try{
@@ -155,7 +167,7 @@ export class RateService {
       const rate = response.data[coin]?.[currency] ?? 0;
       if (rate){
         this.logger.log(`Got rate for ${coin} in ${currency} via api: ${rate}`);
-        this.coinRates[coin] = { ...this.coinRates[coin], [currency]: rate };
+        this.cachedCoinRates[coin] = { ...this.cachedCoinRates[coin], [currency]: rate };
       } 
       return rate; 
     }catch(error){
