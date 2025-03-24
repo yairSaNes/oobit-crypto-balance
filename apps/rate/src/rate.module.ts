87a66ffc8900e0eb0c28e77eb9fab7cb6d02f128
Yair Sa-nes <Yair.sa-nes@harmonicinc.com>
2025-03-24 16:37:22 +0200

    added scheduler to module

--- apps/rate/src/rate.module.ts
+++ apps/rate/src/rate.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { RateController } from './rate.controller';
 import { RateService } from './rate.service';
 import { SharedModule } from '@shared/shared.module';
+import { ScheduleModule } from '@nestjs/schedule';
 
 @Module({
-  imports: [SharedModule],
+  imports: [SharedModule,ScheduleModule.forRoot()],
   controllers: [RateController],
   providers: [RateService],
 })
